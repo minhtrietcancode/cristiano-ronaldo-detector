bdfc655bfffb24519eb68e405ec86f92aea33701
Minh Triet Pham <minhtrietp@student.unimelb.edu.au>
2025-07-10 13:27:52 +0700

    done setting up - run on google collab

---
+++ model/train_yolov8.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+# Load a pre-trained YOLOv8n model
+model = YOLO('yolov8n.pt')
+
+# Define the path to your custom data.yaml
+data_yaml_path = '../My First Project.v1i.yolov8/data.yaml'
+
+# Train the model with your custom dataset
+# You can adjust epochs, image size (imgsz), and other parameters as needed
+results = model.train(data=data_yaml_path, epochs=100, imgsz=640)
+
+# You can optionally save the trained model
+# model.save('my_finetuned_yolov8.pt') 
